e199891169d1f08cd26c3d7c34aeb0623f9f7854
Kevin Moore <moore.kevin.p@gmail.com>
2020-06-10 15:48:06 -0400

    worked more on drag and drop for demo

--- src/components/Demo/Demo.js
+++ src/components/Demo/Demo.js
@@ -4,20 +4,55 @@ import './Demo.css';
 
 export default class Demo extends React.Component {
     state = {
-        chores: [],
+        assignedChores: [],
+        unassignedChores: []
     };
+
+    allowDrop = (ev) => {
+        ev.preventDefault();
+    }
+
+    handleDrag = (ev) => {
+        ev.dataTransfer.setData('text', ev.target.id);
+    }
+
+    //It is correctly tracking new values and updating state
+    //When an element is dragged and dropped, it is also no longer rendering the element that came after it
+    //However, it is removing additional elements
+    //I suspect the splice method on 32 is the issue
+    handleDrop = (ev) => {
+        let newAssignedChores = this.state.assignedChores;
+        let newUnassignedChores = this.state.unassignedChores;
+        ev.preventDefault();
+        let data = ev.dataTransfer.getData('text');
+        console.log('data is ', data)
+        ev.target.appendChild(document.getElementById(data));
+        newAssignedChores.push(newUnassignedChores[data]);
+        console.log('assigned chores are ', newAssignedChores);
+        newUnassignedChores.splice(data, 1);
+        console.log('unassigned chores are ', newUnassignedChores);
+        this.setState({
+            assignedChores: newAssignedChores,
+            unassignedChores: newUnassignedChores
+        })
+    }
 
     renderChoreList = () => {
         console.log('renderChoreList ran')
-        const chores = this.state.chores;
-        console.log('chores are ', chores)
-        const listItems = chores.map((chore) =>
-            <li key={chores.indexOf(chore)}>
+        let chores = this.state.unassignedChores;
+
+        let listItems = chores.map((chore) =>
+            <li 
+                id={chores.indexOf(chore)}
+                className='demochore'
+                draggable='true'
+                onDragStart={this.handleDrag}
+                key={chores.indexOf(chore)}>
                 {chore}
             </li>
         );
         return(
-            <ul className='chorelist'>
+            <ul className='demochorelist'>
                 {listItems}
             </ul>
         );
@@ -25,10 +60,14 @@ export default class Demo extends React.Component {
 
     componentDidMount() {
         this.setState({
-            chores: Store
+            unassignedChores: Store
         });
     };
 
+    //TODOs:
+    //chores can be dragged/dropped
+    //unassign resets chore list
+    //randomize assigns chores
     render() {
         return(
             <div className='demo'>
@@ -36,11 +75,28 @@ export default class Demo extends React.Component {
                     Demo House
                 </h2>
                 <section className='demousers'>
-                    <div className='demouser'>
-                        <span className='demousername'>User1</span>
+                    <div 
+                        id='demouser1'
+                        className='demouser'
+                    >
+                        <p className='demousername'>User1</p>
+                        <ul 
+                            className='demouserchores'
+                            draggable='true'
+                            onDrop={this.handleDrop}
+                            onDragOver={this.allowDrop}
+                        >
+
+                        </ul>
                     </div>
-                    <div className='demouser'>
-                        <span className='demousername'>User2</span>
+                    <div 
+                        id='demouser2'
+                        className='demouser'
+                    >
+                        <p className='demousername'>User2</p>
+                        <ul className='demouserchores'>
+
+                        </ul>
                     </div>
                 </section>
                 <section className='demochores'>
