546d34659c28dab04b323da51fbaf1aaf168c7ab
Kevin Moore <moore.kevin.p@gmail.com>
2020-06-17 13:15:32 -0400

    structure of manage component

--- src/components/Manage/Manage.js
+++ src/components/Manage/Manage.js
@@ -5,6 +5,55 @@ import './Manage.css';
 export default class Manage extends React.Component {
 
     //use props passed from app
+
+    //remove user button doesnt do anything yet
+    renderUsers = (users) => {
+        let usersToRender = users.map(user =>
+                <li
+                   key={user.id}
+                   id={user.id} 
+                   className='manageuser'
+                >
+                    <p className='manageusername'>
+                        {user.username}
+                    </p>
+                    <button
+                        className='manageuserbutton'
+                    >
+                        Remove User
+                    </button>
+                </li>
+        );
+        return(
+            <ul className='manageuserlist'>
+                {usersToRender}
+            </ul>
+        );
+    };
+
+    renderChores = (chores) => {
+        let choresToRender = chores.map(chore =>
+            <li
+                key={chores.indexOf(chore)}
+                id={chores.indexOf(chore)}
+                className='managechore'
+            >
+                <p className='managechorename'>
+                    {chore}    
+                </p>
+                <button
+                    className='managechorebutton'
+                >
+                    Remove Chore
+                </button>
+            </li>
+        );
+        return(
+            <ul className='managechorelist'>
+                {choresToRender}
+            </ul>
+        );
+    };
     
     render() {
         return(
@@ -19,15 +68,23 @@ export default class Manage extends React.Component {
                     <span>
                         Users
                     </span>
-
+                    {this.renderUsers(this.props.users)}
                 </div>
                 <div className='managechores'>
                     <span>
                         Chores
                     </span>
-
+                    {this.renderChores(this.props.chores)}
+                    <Link
+                        to='/addchore'
+                        className='addchorelink'
+                    >
+                        <button>
+                            Add more
+                        </button>
+                    </Link>
                 </div>
             </div>
-        )
+        );
     };
 };
