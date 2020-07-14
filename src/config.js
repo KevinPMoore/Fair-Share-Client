c458bb3f50ad56847d41b6d8a3c568205598c3b4
Kevin Moore <moore.kevin.p@gmail.com>
2020-07-14 15:32:50 -0400

    updated the api endpoint

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export default {
-    API_ENDPOINT: 'https://joli-livre-56448.herokuapp.com/',
+    API_ENDPOINT: 'https://joli-livre-56448.herokuapp.com',
     // local: API_ENDPOINT: 'http://localhost:8000/api',
     TOKEN_KEY: 'fs-client-auth-token',
 };
